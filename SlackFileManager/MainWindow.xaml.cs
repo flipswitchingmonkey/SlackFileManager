f9de5254057db083fdcfd65e59c2a24c6cb17b22
Michael Auerswald <michael.auerswald@gmail.com>
2019-05-20 18:32:44 +0200

    v0.2
    Bugfixes
    Thumbnail / HTML preview
    Deleted Selected (<esc> shortcut)
    Download Selected (<shift>-<d> shortcut)
    Select Folder Dialog
    SlackAPI: added channel to GetFiles
    Channellist Dropdown and query parameter
    Removed sidebar toggle button

--- SlackFileManager/MainWindow.xaml.cs
+++ SlackFileManager/MainWindow.xaml.cs
@@ -18,6 +18,11 @@ using System.ComponentModel;
 using System.Linq.Expressions;
 using System.Collections.ObjectModel;
 using System.Windows.Threading;
+using System.Net;
+using System.IO;
+using System.Collections.Specialized;
+using WK.Libraries.BetterFolderBrowserNS;
+using Syroot.Windows.IO;
 
 namespace SlackFileManager
 {
@@ -27,7 +32,7 @@ namespace SlackFileManager
     public partial class MainWindow : Window
     {
         SlackClient client;
-        // Hardcoded - bad idea
+        public string VERSIONSTRING { get; } = "Slack File Manager v0.2";
 
         public string Oauth_Value
         {
@@ -40,12 +45,14 @@ namespace SlackFileManager
         List<ResponseChannel> responseChannels;
         List<ResponseUser> responseUsers;
         List<ResponseFile> responseFiles;
+        HashSet<string> fileTypes;
 
         int maxFilesTotal, maxFilesPerPage;
         int apiCallDelay = 1250;
 
         struct FileListOptions
         {
+            public string channel;
             public string userId;
             public DateTime? from;
             public DateTime? to;
@@ -105,24 +112,26 @@ namespace SlackFileManager
 
         private void Connect_Click(object sender, RoutedEventArgs e)
         {
+            if (Oauth_Value == "")
+            {
+                MessageBoxResult dialogResult = MessageBox.Show($"Oauth field is empty", "Error", MessageBoxButton.OK);
+                return;
+            }
+
             SetStatus("Connecting...");
             if (ConnectToSlack() == null) { SetStatus("Can't connect. Maybe invalid Oauth?"); return; }
 
-            
             ClearslackFiles();
-            
-            //client.GetChannelList((response) => {
-            //    Dispatcher.BeginInvoke(DispatcherPriority.Input, new Action(() => { AddChannels(response.channels); }));
-            //});
+
             maxFilesTotal = (int)MaxFiles.Value;
             maxFilesPerPage = Math.Min(maxFilesTotal, 100);
 
-            if (ComboUser.Items.Count == 0)
-                GetUsersFromSlack();
+            if (ComboUser.Items.Count == 0) GetUsersFromSlack();
 
             // First Run
             var options = new FileListOptions()
             {
+                channel = (ComboChannels.SelectedItem == null) ? null : (ComboChannels.SelectedItem as ResponseChannel).id,
                 userId = (ComboUser.SelectedItem == null) ? null : (ComboUser.SelectedItem as ResponseUser).id,
                 from = DateFrom.SelectedDate,
                 to = DateTo.SelectedDate,
@@ -132,17 +141,20 @@ namespace SlackFileManager
             };
 
             GetFilesFromSlack(options);
-            //client.GetFiles((response) => {
-            //                    SetStatus("Receiving files...");
-            //                    Dispatcher.BeginInvoke(DispatcherPriority.Input, new Action(() => { AddFiles(response); }));
-            //                }, 
-            //                userId: null,
-            //                from: DateFrom.SelectedDate,
-            //                to: DateTo.SelectedDate,
-            //                count: maxFilesPerPage,
-            //                page: null,
-            //                types: (FileTypes)ComboFileTypes.SelectedItem
-            //);
+
+        }
+
+        private void GetChannelsFromSlack()
+        {
+            if (client == null) return;
+            client.GetChannelList((response) =>
+            {
+                SetStatus("Receiving channel list...");
+                Dispatcher.BeginInvoke(DispatcherPriority.Input, new Action(() => {
+                    AddChannels(response);
+                }));
+
+            });
         }
 
         private void GetUsersFromSlack()
@@ -168,6 +180,7 @@ namespace SlackFileManager
                                     SetStatus($"Receiving page {options.page}...");
                                 Dispatcher.BeginInvoke(DispatcherPriority.Input, new Action(() => { AddFiles(response, options); }));
                             },
+                            channel: options.channel,
                             userId: options.userId,
                             from: options.from,
                             to: options.to,
@@ -175,6 +188,33 @@ namespace SlackFileManager
                             page: options.page,
                             types: options.types
             );
+        }
+
+        private void PopulateFilterByFileType()
+        {
+            fileTypes = new HashSet<string>();
+            fileTypes.Add("");
+            foreach (var file in responseFiles)
+            {
+                fileTypes.Add(file.filetype);
+            }
+            FilterByFileType.ItemsSource = fileTypes;
+            FilterByFileType.Items.Refresh();
+        }
+
+        private void AddChannels(ChannelListResponse response)
+        {
+            responseChannels.Clear();
+            foreach (var channel in response.channels)
+            {
+                responseChannels.Add(ResponseChannel.FromChannel(channel));
+            }
+            ComboChannels.ItemsSource = null;
+            responseChannels.Insert(0, ResponseChannel.GetAllChannel());  // insert the All Users user on top
+            ComboChannels.ItemsSource = responseChannels;
+            ComboChannels.Items.Refresh();
+            ComboChannels.SelectedIndex = 0;
+            SetStatus($"Channellist received. ({responseChannels.Count-1} channels)");
         }
 
         private void AddUsers(UserListResponse response)
@@ -190,7 +230,7 @@ namespace SlackFileManager
             ComboUser.ItemsSource = responseUsers;
             ComboUser.Items.Refresh();
             ComboUser.SelectedIndex = 0;
-            SetStatus($"Userlist received. ({responseUsers.Count} users)");
+            SetStatus($"Userlist received. ({responseUsers.Count-1} users)");
         }
 
         private void AddFiles(FileListResponse response, FileListOptions options)
@@ -209,6 +249,7 @@ namespace SlackFileManager
                 responseFiles.Add(newFile);
             }
             slackFiles.Items.Refresh();
+            PopulateFilterByFileType();
             SetStatus($"Page received. (Received: {response.paging.page} Available: {response.paging.pages})");
             SetProgress((response.paging.page) / ((double)maxFilesTotal/maxFilesPerPage) * 100.0, $"Receiving {response.paging.page} / {Math.Ceiling((double)maxFilesTotal / (double)maxFilesPerPage)}");
             if (responseFiles.Count < maxFilesTotal && response.paging.page < response.paging.pages)
@@ -236,7 +277,67 @@ namespace SlackFileManager
 
         private void slackFiles_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            propertyGrid.SelectedObject = slackFiles.SelectedItem;
+            var selected = slackFiles.SelectedItem as ResponseFile;
+            propertyGrid.SelectedObject = selected;
+            GetPreview(selected);
+            
+        }
+
+        /// <summary>
+        /// Fetch thumbnail image from slack. Automatically picks largest available or hides thumbnail if none.
+        /// </summary>
+        /// <param name="selected"></param>
+        private async void GetPreview(ResponseFile selected)
+        {
+            if (selected == null)
+            {
+                PreviewBrowser.Visibility = Visibility.Hidden;
+                PreviewThumbnail.Visibility = Visibility.Hidden;
+                return;
+            }
+
+            Uri imgUrl;
+            if (selected.thumb_480 != null) { imgUrl = new Uri(selected.thumb_480); }
+            else if (selected.thumb_360 != null) { imgUrl = new Uri(selected.thumb_360); }
+            else if (selected.thumb_160 != null) { imgUrl = new Uri(selected.thumb_160); }
+            else
+            {
+                PreviewThumbnail.Visibility = Visibility.Hidden;
+                PreviewBrowser.Visibility = Visibility.Visible;
+                PreviewBrowser.NavigateToString(selected.preview);
+                return;
+            }
+            
+            var request_data = new NameValueCollection();
+            request_data.Add("Content-Type", "application/x-www-form-urlencoded");
+            request_data.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36");
+
+            var webclient = new WebClient();
+            webclient.Encoding = Encoding.UTF8;
+            webclient.Proxy = WebRequest.DefaultWebProxy;
+            webclient.Headers.Add(HttpRequestHeader.Authorization, "Bearer " + Oauth_Value);
+
+            try
+            {
+                var imageData = await webclient.DownloadDataTaskAsync(imgUrl);
+
+                var bitmapImage = new BitmapImage { CacheOption = BitmapCacheOption.Default };
+                var encoder = new PngBitmapEncoder();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = new MemoryStream(imageData);
+                bitmapImage.EndInit();
+
+                PreviewBrowser.Visibility = Visibility.Hidden;
+                PreviewThumbnail.Visibility = Visibility.Visible;
+                PreviewThumbnail.Source = bitmapImage;
+            }
+            catch
+            {
+                PreviewBrowser.Visibility = Visibility.Hidden;
+                PreviewThumbnail.Visibility = Visibility.Hidden;
+            }
+
+            webclient.Dispose();
         }
 
         private void TogglePropertyGrid_Click(object sender, RoutedEventArgs e)
@@ -252,6 +353,11 @@ namespace SlackFileManager
         }
 
         private void DeleteFile_Click(object sender, RoutedEventArgs e)
+        {
+            DoDelete();
+        }
+
+        private void DoDelete()
         {
             var file = slackFiles.SelectedItem as ResponseFile;
             MessageBoxResult dialogResult = MessageBox.Show($"Are you sure you wish to delete the file: {file.name}", "Are you sure?", MessageBoxButton.YesNo);
@@ -277,6 +383,12 @@ namespace SlackFileManager
             }
         }
 
+        private void RefreshChannels_Click(object sender, RoutedEventArgs e)
+        {
+            if (ConnectToSlack() == null) { SetStatus("Can't connect. Maybe invalid Oauth?"); return; }
+            GetChannelsFromSlack();
+        }
+
         private void RefreshUsers_Click(object sender, RoutedEventArgs e)
         {
             if (ConnectToSlack() == null) { SetStatus("Can't connect. Maybe invalid Oauth?"); return; }
@@ -290,18 +402,12 @@ namespace SlackFileManager
 
         private void ApplyFilter()
         {
-            if (FilterByName.Text == "" && FilterByUserName.Text == "")
-            {
-                slackFiles.Items.Filter = null;
-            }
-            else
-            {
-                slackFiles.Items.Filter = new Predicate<object>(
-                    item => (
-                        ((ResponseFile)item).name.Contains(FilterByName.Text) &&
-                        ((ResponseFile)item).username.Contains(FilterByUserName.Text)
-                        ));
-            }
+            slackFiles.Items.Filter = new Predicate<object>(
+                item => (
+                    ((ResponseFile)item).name.Contains(FilterByName.Text) &&
+                    ((ResponseFile)item).username.Contains(FilterByUserName.Text) &&
+                    ((ResponseFile)item).filetype.Contains(FilterByFileType.SelectedValue.ToString())
+                    ));
         }
 
         private void FilterByUserName_TextChanged(object sender, TextChangedEventArgs e)
@@ -323,6 +429,65 @@ namespace SlackFileManager
             {
                 return;
             }
+        }
+
+        private void DownloadSelected_Click(object sender, RoutedEventArgs e)
+        {
+            DoDownload();
+        }
+
+        private async void DoDownload()
+        {
+            var files = slackFiles.SelectedItems.Cast<ResponseFile>().ToList();
+            if (files == null || files.Count == 0) return;
+
+            var bfb = new BetterFolderBrowser();
+            bfb.Multiselect = false;
+            bfb.Title = "Select Output Folder";
+            bfb.RootFolder = KnownFolders.Downloads.Path;
+
+            var bfbResult = bfb.ShowDialog();
+
+            if (bfbResult == System.Windows.Forms.DialogResult.OK)
+            {
+                if (!Directory.Exists(bfb.SelectedFolder))
+                {
+                    Directory.CreateDirectory(bfb.SelectedFolder);
+                }
+                await DownloadFilesAsync(files, bfb.SelectedFolder);
+            }
+            bfb.Dispose();
+        }
+
+        private async Task DownloadFilesAsync(List<ResponseFile> files, string outputFolder)
+        {
+            var request_data = new NameValueCollection();
+            request_data.Add("Content-Type", "application/x-www-form-urlencoded");
+            request_data.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36");
+
+            var webclient = new WebClient();
+            webclient.Encoding = Encoding.UTF8;
+            webclient.Proxy = WebRequest.DefaultWebProxy;
+            webclient.Headers.Add(HttpRequestHeader.Authorization, "Bearer " + Oauth_Value);
+
+            int currentFile = 0;
+            foreach (var file in files)
+            {
+                try
+                {
+                    string combinedPath = System.IO.Path.Combine(outputFolder, file.name);
+                    SetStatus($"Downloading file {file.name}");
+                    SetProgress((currentFile + 1) / (double)files.Count * 100.0, $"Downloading {currentFile + 1} / {files.Count}");
+                    await webclient.DownloadFileTaskAsync(new Uri(file.url_private_download), combinedPath);
+                }
+                catch
+                {
+                    MessageBox.Show($"Could not download {file.name}");
+                }
+            }
+            SetStatus($"Download finished.");
+            SetProgress(100.0, $"");
+            webclient.Dispose();
         }
 
         private void DeleteFiles(List<ResponseFile> files, int currentFile=0)
@@ -371,6 +536,23 @@ namespace SlackFileManager
         private void SetStatus(string statusText)
         {
             Dispatcher.BeginInvoke(DispatcherPriority.Input, new Action(() => { StatusLabel.Content = statusText; }));
+        }
+
+        private void FilterByFileType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ApplyFilter();
+        }
+
+        private void SlackFiles_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Delete)
+            {
+                DoDelete();
+            }
+            else if (e.Key == Key.D && ((Keyboard.Modifiers & ModifierKeys.Shift) == ModifierKeys.Shift))
+            {
+                DoDownload();
+            }
         }
 
         private void SetProgress(double percent, string label="")
--- SlackFileManager/ResponseChannel.cs
+++ SlackFileManager/ResponseChannel.cs
@@ -3,16 +3,74 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SlackAPI;
 
 namespace SlackFileManager
 {
     public class ResponseChannel
     {
+        // from Conversation
+        public string id { get; set; }
+        public DateTime created { get; set; }
+        public DateTime last_read { get; set; }
+        public bool is_open { get; set; }
+        public bool is_starred { get; set; }
+        public int unread_count { get; set; }
+        public Message latest { get; set; }
+
+        // from Channel
         public string name { get; set; }
+        public string creator { get; set; }
+
+        public bool is_archived { get; set; }
+        public bool is_member { get; set; }
+        public bool is_general { get; set; }
+        public bool is_channel { get; set; }
+        public bool is_group { get; set; }
+        //Is this deprecated by is_open?
+        public bool IsPrivateGroup { get { return id != null && id[0] == 'G'; } }
+
+        public int num_members { get; set; }
+        public OwnedStampedMessage topic { get; set; }
+        public OwnedStampedMessage purpose { get; set; }
+
+        public string[] members { get; set; }
 
         public ResponseChannel(string name="blank")
         {
             this.name = name;
+        }
+
+        public static ResponseChannel GetAllChannel()
+        {
+            var u = new ResponseChannel();
+            u.name = "All Channels";
+            u.id = null;
+            return u;
+        }
+
+        public static ResponseChannel FromChannel(Channel channel)
+        {
+            var rs = new ResponseChannel();
+            rs.id = channel.id;
+            rs.created = channel.created;
+            rs.last_read = channel.last_read;
+            rs.is_open = channel.is_open;
+            rs.is_starred = channel.is_starred;
+            rs.unread_count = channel.unread_count;
+            rs.latest = channel.latest;
+            rs.name = channel.name;
+            rs.creator = channel.creator;
+            rs.is_archived = channel.is_archived;
+            rs.is_member = channel.is_member;
+            rs.is_general = channel.is_general;
+            rs.is_channel = channel.is_channel;
+            rs.is_group = channel.is_group;
+            rs.num_members = channel.num_members;
+            rs.topic = channel.topic;
+            rs.purpose = channel.purpose;
+            rs.members = channel.members;
+            return rs;
         }
     }
 }
